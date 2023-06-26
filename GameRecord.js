8fb1d77a7459fb681064c9126004e48bb3744bb2
jtstrunk <108699983+jtstrunk@users.noreply.github.com>
2023-06-26 15:59:25 -0400

    Style Changes

--- GameRecord.js
+++ GameRecord.js
@@ -1,8 +1,19 @@
-let domGames = document.querySelector("#domGames");
-let spans = document.querySelectorAll("span")
 
+document.addEventListener("DOMContentLoaded", function() {
+    let domGames = document.querySelector("#domGames");
+    let spans = document.querySelectorAll("span")
+  
+    for (var i = 0; i < spans.length; i++) {
+        var span = spans[i];
+  
+        if (span.innerText === "Score: None") {
+            let before = spans[i - 1];
+            console.log(before)
+            if(before){
+                before.classList.add("Hide");
+            }
+            span.classList.add("Hide");
+        }
+    }
 
-for (spans in domGames){
-    if(spans.innertext = "None")
-        spans.classList.add("Hide");
-}
+});  
